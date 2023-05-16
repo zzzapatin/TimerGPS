3e8cf41b4da4cefdced690ff1dc121f0093402c2
andres zapata <andres.zapata1@gmail.com>
2023-05-16 16:37:52 -0400

    -Encapsule obtencion de permisos y cambio de configuracion de GPS en funciones. -Se agrego chequeo de permisos al empezar locationUpdates.

--- app/src/main/java/com/example/timergps/MainActivity.kt
+++ app/src/main/java/com/example/timergps/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.timergps
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -15,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.timergps.ui.theme.TimerGPSTheme
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -34,78 +38,32 @@ class MainActivity : ComponentActivity() {
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
     private var requestingLocationUpdates: Boolean = false
-    lateinit var mCurrentLocation: Location
+    private lateinit var mCurrentLocation: Location
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        //Pedir permisos
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            when {
-                permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    // Precise location access granted.
-                }
-                permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    // Only approximate location access granted.
-                } else -> {
-                // No location access granted.
-                }
-            }
-        }
-        locationPermissionRequest.launch(arrayOf(
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION))
+        obtainPermissionRequest()
 
+        requestLocationSettings()
 
-        val locationRequestBuilder = LocationRequest.Builder(
-            Priority.PRIORITY_HIGH_ACCURACY,
-            1000
-        )
-        locationRequestBuilder.setMinUpdateIntervalMillis(5000)
-        locationRequest = locationRequestBuilder.build()
-        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
-        val client: SettingsClient = LocationServices.getSettingsClient(this)
-        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-
-        //ver si esta encendido el GPS
-        task.addOnSuccessListener { locationSettingsResponse ->
-            // All location settings are satisfied. The client can initialize
-            // location requests here.
-            requestingLocationUpdates = true
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location : Location? ->
-                    // Got last known location. In some rare situations this can be null.
-                    if (location != null) {
-                        mCurrentLocation = location
-                    }
-                }
-
-        }
-        //si no lo esta preguntar que lo encienda
-        task.addOnFailureListener { exception ->
-            if (exception is ResolvableApiException){
-                // Location settings are not satisfied, but this can be fixed
-                // by showing the user a dialog.
-                try {
-                    // Show the dialog by calling startResolutionForResult(),
-                    // and check the result in onActivityResult().
-                    exception.startResolutionForResult(this@MainActivity,
-                        REQUEST_CHECK_SETTINGS)
-
-                } catch (sendEx: IntentSender.SendIntentException) {
-                    // Ignore the error.
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    mCurrentLocation = location
+                    Log.d("MyLocation", mCurrentLocation.toString())
+                }else{
+                    Log.d("MyLocation", "fail")
                 }
             }
-        }
 
         locationCallback = object : LocationCallback() {
-              fun onLocationResultTemp(locationResult: LocationResult?) {
+             fun onLocationResultTemp(locationResult: LocationResult?) {
                 locationResult ?: return
                 for (location in locationResult.locations){
-                    // Update UI with location data
-                    mCurrentLocation = location
+                    Log.d("MyLocation", location.toString())
                 }
             }
         }
@@ -116,33 +74,89 @@ class MainActivity : ComponentActivity() {
             TimerGPSTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting(mCurrentLocation.altitude.toString() + mCurrentLocation.longitude.toString())
+                    Greeting("Pepe")
                 }
             }
         }
+    }
+
+    //Pedir permisos
+    private fun obtainPermissionRequest(){
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+
+    private fun requestLocationSettings(){
+
+        val locationRequestBuilder = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+        locationRequestBuilder.setMinUpdateIntervalMillis(5000)
+        locationRequest = locationRequestBuilder.build()
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        //ver si esta encendido el GPS
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize. location requests here.
+            requestingLocationUpdates = true
+        }
+        //si no lo esta preguntar que lo encienda
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity, REQUEST_CHECK_SETTINGS)
+
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
     }
 
     override fun onResume() {
         super.onResume()
         if (requestingLocationUpdates) startLocationUpdates()
     }
-
-    @SuppressLint("MissingPermission")
     private fun startLocationUpdates() {
+        val fineLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        val coarseLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if (fineLocationPermission != PackageManager.PERMISSION_GRANTED
+            && coarseLocationPermission != PackageManager.PERMISSION_GRANTED)
+        {
+            obtainPermissionRequest()
+        }
+
         fusedLocationClient.requestLocationUpdates(locationRequest,
             locationCallback,
             Looper.getMainLooper())
     }
-
     override fun onPause() {
         super.onPause()
         stopLocationUpdates()
     }
-
     private fun stopLocationUpdates() {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
-
 
 }
 
